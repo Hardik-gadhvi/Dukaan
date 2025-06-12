56145ba76cb58e66df55fd7e36bdc0a49e4bdb7b
Hardik-gadhvi <gadhvi.hardik1159@gmail.com>
2025-06-12 22:33:10 +0530

    Added Error Controller, Exception Middleware, CORS

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ErrorController : BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("It's Bad Request!");
+        }
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception("This is an internal server error!");
+        }
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationError(CreateProductDTO product)
+        {
+            return Ok();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -10,17 +11,22 @@ builder.Services.AddDbContext<StoreContext>(options =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-;
+
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
-app.MapControllers();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
-try
-{
+app.MapControllers(); 
+
+try 
+{ 
     using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<StoreContext>();
