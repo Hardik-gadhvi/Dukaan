810025b9ea6daeb938aed5c6eb30ec502c418770
Hardik-gadhvi <gadhvi.hardik1159@gmail.com>
2025-06-13 19:10:29 +0530

    Angular setup for Client app, SSL for angular app URL added

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
-
+ 
 app.MapControllers(); 
 
 try 
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    pictureUrl: string;
+    description: string;
+    brand: string;
+    type: string;
+    quantityInStock: number;
+}
