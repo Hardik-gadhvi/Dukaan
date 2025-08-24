1f81280229a0eceb69373d1513e15bb69cb70e47
Hardik-gadhvi <gadhvi.hardik1159@gmail.com>
2025-08-24 18:17:20 +0530

    Added Identity(Authentication and Authorization) at API side

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -23,16 +24,20 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
          return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 builder.Services.AddControllers();
  
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
  
 app.MapControllers(); 
+app.MapGroup("api").MapIdentityApi<AppUser>();
 
 try 
 { 
